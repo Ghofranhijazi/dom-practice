d9ad73c0eea6474b0c39553fa4db2103b0996a38
Ghofranhijazi <ghofranhijazi02@gmail.com>
2025-01-02 10:59:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// selecting where to add the element
+let list = document.querySelector("ul");
+// creating an element to add 
+let item = document.createElement("li");
+// adding element to the DOM
+list.appendChild(item);
+// writing contant in the element (optional)
+//  item.textContent="hareth";
